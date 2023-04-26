9da88b9c97a5d044606d52c03b8b23c29d4723a1
Daniel Diaz <danieldiaz-123@hotmail.com>
2023-04-25 22:06:45 -0300

    Cards hardcodeadas

---
+++ Client/src/components/Cards/Cards.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from './Cards.module.css';
+
+export default function Cards({ id, name, image, price }) {
+    return (
+
+        <div>
+            <div className={styles.card}>
+
+                <h2>MOTHERBOARD RANDOM</h2>
+
+                <h4>PRICE: 3422</h4>
+            </div>
+
+            <div className={styles.card}>
+
+                <h2>MOUSE RANDOM</h2>
+
+                <h4>PRICE: 23452</h4>
+            </div>
+
+            <div className={styles.card}>
+
+                <h2>MONITOR RANDOM</h2>
+
+                <h4>PRICE: 3463</h4>
+            </div>
+
+
+        </div >
+
+    )
+}
