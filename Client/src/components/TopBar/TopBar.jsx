e9522e01bddb8ca7da23934c609d55c7e20667f3
Franco Inca <francoinca1604@gmail.com>
2023-04-25 22:01:46 -0500

    Added TopBar to Landing Page

--- Client/src/components/TopBar/TopBar.jsx
+++ Client/src/components/TopBar/TopBar.jsx
@@ -1,7 +1,13 @@
+import Styles from './TopBar.module.css';
 const TopBar = () => {
   return (
-    <div>
-      <img src="https://i.imgur.com/3Rl14iv.png" alt="Component Corner" />
+    <div className={Styles.container}>
+      <div>
+        <img src="https://i.imgur.com/3Rl14iv.png" alt="Component Corner" />
+      </div>
+      <div>
+        <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" alt="cart" />
+      </div>
     </div>
   );
 };
