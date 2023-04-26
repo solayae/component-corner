9e69905020da2b35377cad4740ceec848f4d069b
carlos carabajal <carjuancara@gmail.com>
2023-04-26 18:47:01 -0300

    cree ruta para actualizar usuarios

--- Api/src/handler/usersHandler.js
+++ Api/src/handler/usersHandler.js
@@ -1,67 +1,108 @@
-
-const { Usuario } = require('../db');
+const { Usuario } = require("../db");
 
 const handleUsersAll = async (req, res) => {
-  const { name } = req.query
+  const { name } = req.query;
   try {
     if (!name) {
-      const allUsers = await Usuario.findAll()
-      return res.status(200).json(allUsers)
+      const allUsers = await Usuario.findAll();
+      return res.status(200).json(allUsers);
     } else {
       const allUsers = await Usuario.findAll({
         where: {
           name: { [Op.substring]: name },
-        }
-      })
-      return res.status(200).json(allUsers)
+        },
+      });
+      return res.status(200).json(allUsers);
     }
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(400).json({ error: error.message });
   }
-}
-
+};
 
 const handleUserById = async (req, res) => {
   const { UsersId } = req.params;
-try {
-  const user = await Usuario.findByPk(UsersId)
-  return res.status(200).json(user)
-  
-} catch (error) {
-  res.status(400).json({error:error.message})
-}
-  
-}
-
+  try {
+    const user = await Usuario.findByPk(UsersId);
+    return res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 const handleUserCreate = async (req, res) => {
-  const { email, name, password, favorite, direction, cart } = req.body
+  const { email, name, password, favorite, direction, cart } = req.body;
   try {
-    if (!email || !password) return res.status(400).json({ error: "faltas ingresar email y/o password" })
+    if (!email || !password)
+      return res
+        .status(400)
+        .json({ error: "faltas ingresar email y/o password" });
     const [newUser, create] = await Usuario.findOrCreate({
       where: {
-        email:email,
+        email: email,
       },
       defaults: {
         name,
         password,
         favorite,
         direction,
-        cart
+        cart,
+      },
+    });
+    if (!create)
+      return res.status(200).json({ message: "Ya existe el usuario" });
+    return res
+      .status(201)
+      .json({ message: "El usuario se ha creado con éxito!" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+/* 
+if (!id) return res.status(400).json({ error: 'falta el ID' })
+    const updateProduct = await Products.update({
+      image,
+      name,
+      brand,
+      detail,
+      price,
+      category,
+      stock,
+    }, {
+      where: {
+        id: id
       }
     })
-    if (!create) return res.status(200).json({ message: 'Ya existe el usuario' })
-    return res.status(201).json({message: 'El usuario se ha creado con éxito!'})
+*/
 
+const handleUpdateUser = async (req, res) => {
+  const { email, name, password, favorite, direction, cart } = req.body;
+  try {
+    if (!email) return res.status(400).json({ error: "falta el EMAIL" });
+    const updateUser = await Usuario.update(
+      {
+        name,
+        password,
+        favorite,
+        direction,
+        cart,
+      },
+      {
+        where: { email: email },
+      }
+    );
+
+    return updateUser[0] > 0
+      ? res
+          .status(200)
+          .json({ message: "Se actualizo el usuario correctamente!" })
+      : res.status(400).json({ message: "No se actualizo ningun usuario" });
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(400).json({ error: error.message });
   }
-
-}
-
-
+};
 module.exports = {
   handleUsersAll,
   handleUserById,
-  handleUserCreate
-}
+  handleUserCreate,
+  handleUpdateUser,
+};
--- Api/src/routes/usersRouter.js
+++ Api/src/routes/usersRouter.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const usersRouter = Router();
-const { handleUsersAll, handleUserById, handleUserCreate } = require('../handler/usersHandler')
+const { handleUsersAll, handleUserById, handleUserCreate, handleUpdateUser } = require('../handler/usersHandler')
 
 
 usersRouter.get('/', handleUsersAll)
 usersRouter.get('/:UsersId', handleUserById)
-usersRouter.post('/',handleUserCreate)
+usersRouter.post('/', handleUserCreate)
+usersRouter.put('/',handleUpdateUser)
 
 
 module.exports = usersRouter
