550daac957c57c0f034fdd0b1d86b2b8e82c4514
carlos carabajal <carjuancara@gmail.com>
2023-04-26 17:50:42 -0300

    hize ruta para crear un usuario

--- Api/src/handler/usersHandler.js
+++ Api/src/handler/usersHandler.js
@@ -1,5 +1,5 @@
 
-const { Products } = require('../db');
+const { Usuario } = require('../db');
 
 
 
@@ -15,11 +15,52 @@ const handleUserById = async (req, res) => {
   
 }
 
+/* 
+{
+      where: {
+        name: name,
+      },
+      defaults: {
+        image,
+        name,
+        brand,
+        detail,
+        price,
+        category,
+        stock
+      }
+    }
+*/
 
+
+const handleUserCreate = async (req, res) => {
+  const { email, name, password, favorite, direction, cart } = req.body
+  try {
+    if (!email || !password) return res.status(400).json({ error: "faltas ingresar email y/o password" })
+    const [newUser, create] = await Usuario.findOrCreate({
+      where: {
+        email:email,
+      },
+      defaults: {
+        name,
+        password,
+        favorite,
+        direction,
+        cart
+      }
+    })
+    if (!create) return res.status(200).json({ message: 'Ya existe el usuario' })
+    return res.status(201).json({message: 'El usuario se ha creado con éxito!'})
+
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+
+}
 
 
 module.exports = {
   handleUsersAll,
   handleUserById,
-
+  handleUserCreate
 }
--- Api/src/routes/usersRouter.js
+++ Api/src/routes/usersRouter.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
 const usersRouter = Router();
-const { handleUsersAll, handleUserById } = require('../handler/usersHandler')
-handleUsersAll,
-  handleUserById,
+const { handleUsersAll, handleUserById, handleUserCreate } = require('../handler/usersHandler')
+
 
 usersRouter.get('/', handleUsersAll)
 usersRouter.get('/:UsersId', handleUserById)
+usersRouter.post('/',handleUserCreate)
 
 
 module.exports = usersRouter
