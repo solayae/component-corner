caaba257647856ddd19e96534b3e7972c59d813c
Franco Inca <francoinca1604@gmail.com>
2023-04-27 18:39:14 -0500

    Fixed front not rendering due redux error

--- Client/src/components/Cards/Cards.jsx
+++ Client/src/components/Cards/Cards.jsx
@@ -1,13 +1,28 @@
-/* eslint-disable react/jsx-key */
-import { useThemeProps } from '@mui/material';
-import styles from './Cards.module.css';
+import PropTypes from 'prop-types';
 
 export default function Cards(props) {
-    const products = props.products
-    console.log(products)
-    return (
-        <div>
-            {products.map((e)=>(<h1>{e.name}</h1>))}
+  const products = props.products;
+
+  return (
+    <div>
+      {products.map((e) => (
+        <div key={e.id}>
+          <img src={e.image} alt={e.name} />
+          <h1>{e.name}</h1>
+          <p>Marca: {e.brand}</p>
+          <p>En Stock: {e.stock}</p>
+          <h2>${e.price}</h2>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
+Cards.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      image: PropTypes.string,
+      id: PropTypes.string,
+    })
+  ).isRequired,
+};
