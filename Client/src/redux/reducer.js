db2132244ff2f504786b142368a82bc4ef068952
Franco Inca <francoinca1604@gmail.com>
2023-04-26 19:54:02 -0500

    Added redux

---
+++ Client/src/redux/reducer.js
@@ -0,0 +1,16 @@
+import {GET_ALL_PRODUCTS} from './actions';
+
+const initialState = {
+  products: [],
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ALL_PRODUCTS:
+      return {...state, products: action.payload};
+    default:
+      return {...state};
+  }
+};
+
+export default rootReducer;
