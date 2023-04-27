59dc91b13645ebfbdc3106cf4b9782b5ecac85f1
carlos carabajal <carjuancara@gmail.com>
2023-04-27 07:32:05 -0300

    modularize ruta product

--- Api/src/controllers/productsControllers.js
+++ Api/src/controllers/productsControllers.js
@@ -1,19 +1,51 @@
-require('dotenv').config();
-const { Product, Usuario } = require('../db');
+const { Products } = require("../db");
+const { articulos } = require("../Utils/Productos");
 
-
-const getAllProduct = async () => {
-    const dbAllProduct = await Product.findAll({ include: Usuario })
-    return dbAllRaza
-}
-  
 const getProductByName = async (name) => {
-    const allProduct = await Product.findAll({
+  const allProduct = await Products.findAll({
+    where: {
+      name: { [Op.substring]: name },
+    },
+  });
+  return allProduct;
+};
+
+async function addProduct() {
+  const addArticulos = await Products.bulkCreate(articulos);
+  return addArticulos;
+}
+
+async function deleteProduct(productsId) {
+ const updateProduct= await Products.update({ delete: true }, {
       where: {
-        name: { [Op.substring]: name }
+        id: productsId
+      }
+ })
+  return updateProduct
+}
+
+async function updateProducts(image, name, brand, detail, price, category, stock, id) {
+  const updateProduct = await Products.update(
+      {
+        image,
+        name,
+        brand,
+        detail,
+        price,
+        category,
+        stock,
       },
-      include: Usuario 
-    }
-    )
-    return allRazaDB
-  }
+      {
+        where: {
+          id: id,
+        },
+      }
+  );
+  return updateProduct
+}
+module.exports = {
+  addProduct,
+  getProductByName,
+  deleteProduct,
+  updateProducts
+};
