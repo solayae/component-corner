1eb77267d695925a18beae845dc63e57bb2e8f20
Franco Inca <francoinca1604@gmail.com>
2023-04-27 19:07:16 -0500

    Fixed Landing Page

--- Client/src/views/LandingPage/LandingPage.jsx
+++ Client/src/views/LandingPage/LandingPage.jsx
@@ -1,34 +1,59 @@
-import TopBar from '../../components/TopBar/TopBar';
-
+import Topbar from '../../components/Topbar/Topbar';
+import {getAllProducts} from '../../redux/actions';
+import Cards from '../../components/Cards/Cards';
 import Style from './LandingPage.module.css';
-const LandingPage = () => {
+import {useEffect, useState} from 'react';
+import {connect} from 'react-redux';
+
+function mapStateToProps(state) {
+  return {
+    products: state.products,
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    getAllProducts: function () {
+      return dispatch(getAllProducts());
+    },
+  };
+}
+
+const LandingPage = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(function LandingPage(props) {
+  const [recommendedProducts, setRecommended] = useState([]);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    const productsToState = async () => {
+      try {
+        if (!props.products.length) await props.getAllProducts();
+        setRecommended([...props.products.slice(0, 5)]);
+        setMounted(true);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    productsToState();
+    //eslint-disable-next-line
+  }, [mounted]);
   return (
     <div className={Style.landingPage}>
-      <TopBar />
+      <Topbar />
       <div className={Style.banner}>
         <div className={Style.banner__TextContainer}>
           <h1>¡Bienvenido a Component Corner!</h1>
           <p>Todo tipo de componentes y periféricos de calidad a su disposición.</p>
         </div>
       </div>
-      <div className={Style.menuContanier}>
-        <div className={Style.menuTrigger}>
-          <h1>Categorias</h1>
-          <div className={Style.dropdownMenu}>
-            <ul>
-              <li>
-                <p>🎧</p>
-                <p>Periféricos</p>
-              </li>
-              <li>
-                <p>💻</p>
-                <p>Componentes</p>
-              </li>
-            </ul>
-          </div>
-        </div>
+      <div className={Style.menuContanier}></div>
+      <div className={Style.recommendations}>
+        <h1>Recomendaciones:</h1>
+        <Cards products={recommendedProducts} />
       </div>
     </div>
   );
-};
+});
+
 export default LandingPage;
