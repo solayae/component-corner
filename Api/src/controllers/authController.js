89db5db07398b5d0a79a31f099658c42d9ef19f9
billyandryus <billyandrys@gmail.com>
2023-04-29 10:42:49 -0500

    add file authController

--- Api/src/controllers/authController.js
+++ Api/src/controllers/authController.js
@@ -1,1 +1,84 @@
-const { Roles, Usuarios } = require('../db')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const { Role, Usuario } = require('../db')
+const confing = require('../config/authConfig')
+const { Op } = require('sequeize')
+const rolDefault = 1
+
+exports.sinup = (req, res) =>{
+    Usuario.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 12)
+    })
+    .then(usuario =>{
+        if(req.body.roles){
+            Role.findAll({
+                where:{
+                    name:{
+                        [Op.or] : req.body.roles    
+                    }
+                }
+            })
+            usuario.setRoles([rolDefault])
+            .them(()=>{
+                res.send({message : 'Usuario resgistrado con exito!'})
+            })
+        }
+
+    })
+    .catch(()=>{
+        res.status(500).send(err=>{message: err.message})
+    })
+    
+}
+
+
+exports.singin = (req, res)=>{
+    Usuario.findOne({
+        where:{
+            email:req.body.email
+        }
+    })
+    .then(usuario=>{
+        if(!usuario){
+            return res.status(404).send({message:'usuario no exite'})
+        }
+        const passwordValidate = bcrypt.compareSync(
+            req.body.password,
+            usuario.password
+        )
+        if(!passwordValidate){
+            return res.status(401).send({
+                accessToken:null,
+                message:'password es invalido'
+            })
+        }
+
+        const token = jwt.sing({id:usuario.id}, confing.secret, {
+            expiresIn:86400
+        })
+        const authorities = []
+        usuario.getRoles()
+            .then(roles=>{
+                for(let i = 0; i < roles.length; i++){
+                    authorities.push('Role' + roles[i].name.toUpperCase())
+                }
+            res.status(200).send({
+                id : usuario.id,
+                name: usuario.name,
+                roles:authorities,
+                accessToke:toke
+            })
+            
+            })
+            
+    })
+    .catch(err =>{
+        res.status(500).send({
+            message: err.message
+        })
+    })
+}
+
+
--- Api/src/db.js
+++ Api/src/db.js
@@ -30,15 +30,15 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Usuario, Products, Roles } = sequelize.models;
+const { Usuario, Products, Role } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
 Products.belongsToMany(Usuario, { through: 'cart' });
 Usuario.belongsToMany(Products, { through: 'cart' });
-Roles.belongsToMany(Usuario, { through: 'user_roles' });
-Usuario.belongsToMany(Roles, { through: 'user_roles' });
+Role.belongsToMany(Usuario, { through: 'user_roles' });
+Usuario.belongsToMany(Role, { through: 'user_roles' });
 
 
 module.exports = {
