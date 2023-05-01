b8b529b1dfeb35b2041c1192ae4d5d3d6b201deb
Franco Inca <francoinca1604@gmail.com>
2023-05-01 13:31:27 -0500

    feat(App): Added Redux connection to App

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,19 +1,39 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import LandingPage from './views/LandingPage/LandingPage';
 import Footer from './components/Footer/Footer.jsx';
 import Topbar from './components/Topbar/Topbar.jsx';
 import Detail from './views/Detail/Detail';
 import SignInPage from './views/SignInPage/SignInPage';
+import {useDispatch} from 'react-redux';
+import {getAllProducts} from './redux/actions';
+import {useEffect, useState} from 'react';
 
 function App() {
+  const [mounted, setMounted] = useState(false);
+  const dispatch = useDispatch();
+  const getProducts = async () => {
+    return dispatch(getAllProducts());
+  };
+  useEffect(() => {
+    const productsToState = async () => {
+      try {
+        await getProducts();
+        setMounted(true);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    productsToState();
+    //eslint-disable-next-line
+  }, [mounted]);
   return (
-    <div className='App'>
+    <div className="App">
       <Topbar />
       <Routes>
-        <Route exact path='/' element={<LandingPage />} />
-        <Route path='/products/:id' element={<Detail />} />
-        <Route path='/registrarse' element={<SignInPage />} />
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/products/:id" element={<Detail />} />
+        <Route path="/registrarse" element={<SignInPage />} />
       </Routes>
       <Footer />
     </div>
