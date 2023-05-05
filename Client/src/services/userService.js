51e9a54311181ad483b6c89220614a10adfccf62
Sebas Olaya <sebastianolayae@gmail.com>
2023-05-04 23:45:11 -0500

    new backend

--- Client/src/services/userService.js
+++ Client/src/services/userService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import authHeader from './authHeader'
 
 // const API_URL = 'http://localhost:3001/api/'
-const API_URL = 'https://component-corner-production.up.railway.app/products/api/'
+const API_URL = '/products/api/'
 const PUBLIC = 'all'
 const USER = 'user'
 const MOD = 'mod'
