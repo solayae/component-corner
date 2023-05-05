a2605d6856a2488353d90406ac2b57d09a849296
Sebas Olaya <sebastianolayae@gmail.com>
2023-05-05 00:03:51 -0500

    new backend

--- Client/src/services/userService.js
+++ Client/src/services/userService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import authHeader from './authHeader'
 
 // const API_URL = 'http://localhost:3001/api/'
-const API_URL = '/products/api/'
+const API_URL = '/api/'
 const PUBLIC = 'all'
 const USER = 'user'
 const MOD = 'mod'
