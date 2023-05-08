b3f72c0751d6a2d07a2d9ceb9b680927f8e1aa64
Guido <guidofraticelli22@gmail.com>
2023-05-07 23:53:52 -0300

    Pasarela finished

--- Client/src/views/Cart/Cart.jsx
+++ Client/src/views/Cart/Cart.jsx
@@ -1,65 +1,83 @@
-import styles from './Cart.module.css';
+import styles from "./Cart.module.css";
+import axios from "axios";
 
 function Cart({ cart, setCart }) {
+  const handleIncrement = (id) => {
+    setCart((product) => {
+      return product.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            quantity:
+              item.quantity === item.stock ? item.quantity : item.quantity + 1,
+          };
+        } else {
+          return item;
+        }
+      });
+    });
+  };
 
-    const handleIncrement = (id) => {
-        setCart((product) => {
-            return product.map((item) => {
-                if (item.id === id) {
-                    return { ...item, quantity: item.quantity === item.stock ? item.quantity : item.quantity + 1 }
-                } else {
-                    return item;
-                }
-            })
-        })
-    };
+  const handleDecrement = (id) => {
+    setCart((currItem) => {
+      if (currItem.find((item) => item.id === id)?.quantity === 1) {
+        return currItem.filter((item) => item.id !== id);
+      } else {
+        return currItem.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  };
 
-
-    const handleDecrement = (id) => {
-        setCart((currItem) => {
-            if (currItem.find((item) => item.id === id)?.quantity === 1) {
-                return currItem.filter((item) => item.id !== id)
-            } else {
-                return currItem.map((item) => {
-                    if (item.id === id) {
-                        return { ...item, quantity: item.quantity - 1 }
-                    } else {
-                        return item;
-                    }
-                })
-            }
-        })
-    }
-
+  return cart?.map((product) => {
     return (
-        cart?.map((product) => {
-            return <div className={styles.cartContent} key={product.id}>
+      <div className={styles.cartContent} key={product.id}>
+        <img src={product.image}></img>
 
-                <img src={product.image}></img>
+        <div className={styles.productName}>Nombre: {product.name}</div>
 
-                <div className={styles.productName}>
-                    Nombre: {product.name}
-                </div>
+        <div className={styles.productStock}>Stock: {product.stock}</div>
 
-                <div className={styles.productStock}>
-                    Stock: {product.stock}
-                </div>
+        <div className={styles.productPrice}>${product.price}</div>
 
-                <div className={styles.productPrice}>
-                    ${product.price}
-                </div>
+        <div className={styles.cartCounter}>
+          <button
+            className={styles.cartButton}
+            onClick={() => handleIncrement(product.id)}
+          >
+            +
+          </button>
+          <div className={styles.productQuantity}>{product.quantity}</div>
+          <button
+            className={styles.cartButton}
+            onClick={() => handleDecrement(product.id)}
+          >
+            -
+          </button>
+        </div>
 
-                <div className={styles.cartCounter}>
-                    <button className={styles.cartButton} onClick={() => handleIncrement(product.id)}>+</button>
-                    <div className={styles.productQuantity}>
-                        {product.quantity}
-                    </div>
-                    <button className={styles.cartButton} onClick={() => handleDecrement(product.id)}>-</button>
-                </div>
-
-            </div >
-        })
-    )
+        <div>
+          <button
+            onClick={() =>
+              axios
+                .post("http://localhost:3001/payment", product)
+                .then(
+                  (res) =>
+                    (window.location.href = res.data.response.body.init_point)
+                )
+            }
+          >
+            Comprar
+          </button>
+        </div>
+      </div>
+    );
+  });
 }
 
-export default Cart
+export default Cart;
