7e6cbd2ba541fe338d66b997a72d273cfa832da0
Franco Inca <115196374+FrancoInca@users.noreply.github.com>
2023-05-07 22:59:58 -0500

    Update app.js

--- Api/src/app.js
+++ Api/src/app.js
@@ -23,7 +23,7 @@ server.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
-    "x-access-token, Origin, X-Requested-With, Content-Type, Accept"
+    "x-access-token, Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
