40bf4fcb38454986e0d85d30b9a90beb0ab3e31b
billyandryus <billyandrys@gmail.com>
2023-05-09 12:34:07 -0500

    modified Navbar

--- Client/src/components/NavBar/NavBar.jsx
+++ Client/src/components/NavBar/NavBar.jsx
@@ -2,40 +2,20 @@ import {useState, useEffect, useCallback} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {Link, useLocation} from 'react-router-dom';
 
-<<<<<<< HEAD
-import { useState, useEffect, useCallback } from "react";
-import { useDispatch } from "react-redux";
-import {  Link, useLocation } from "react-router-dom";
-
-
-import { logout } from "../../redux/actions";
-import { clearMessage } from "../../redux/actions";
-import EventBus from "../../common/EventBus";
-
-
-=======
 import {logout} from '../../redux/actions';
 import {clearMessage} from '../../redux/actions';
 import EventBus from '../../common/EventBus';
->>>>>>> 4827aa5a03443d463fefe8713dc5a3d64dadfa81
 
 const NavBar = () => {
   const [showModeratorBoard, setShowModeratorBoard] = useState(false);
   const [showAdminBoard, setShowAdminBoard] = useState(false);
-<<<<<<< HEAD
   //const { user: currentUser } = useSelector((state) => state.user);
-=======
   const {user: currentUser} = useSelector((state) => state.user);
->>>>>>> 4827aa5a03443d463fefe8713dc5a3d64dadfa81
   const dispatch = useDispatch();
   let location = useLocation();
   const user = localStorage.getItem('user')
   useEffect(() => {
-<<<<<<< HEAD
-    if (["/", "/home"].includes(location.pathname)) {
-=======
     if (['/home', '/register'].includes(location.pathname)) {
->>>>>>> 4827aa5a03443d463fefe8713dc5a3d64dadfa81
       dispatch(clearMessage()); // clear message when changing location
     }
   }, [dispatch, location]);
@@ -45,15 +25,9 @@ const NavBar = () => {
   }, [dispatch]);
 
   useEffect(() => {
-<<<<<<< HEAD
     if (user.roles) {
       setShowModeratorBoard(user?.roles.includes("ROLE_MODERATOR"));
       setShowAdminBoard(user?.roles.includes("ROLE_ADMIN"));
-=======
-    if (currentUser?.roles) {
-      setShowModeratorBoard(currentUser?.roles.includes('ROLE_MODERATOR'));
-      setShowAdminBoard(currentUser?.roles.includes('ROLE_ADMIN'));
->>>>>>> 4827aa5a03443d463fefe8713dc5a3d64dadfa81
     } else {
       setShowModeratorBoard(false);
       setShowAdminBoard(false);
@@ -97,37 +71,6 @@ const NavBar = () => {
             </li>
           )}
 
-<<<<<<< HEAD
-            {user && (
-                <li className="nav-item">
-                <Link to={"/user"} className="nav-link">
-                    Usuario
-                </Link>
-                </li>
-            )}
-            </div>
-
-            {user ? (
-            <div className="navbar-nav ml-auto">
-                <li className="nav-item">
-                <Link to={"/profile"} className="nav-link">
-                   Mi perfil
-                </Link>
-                </li>
-                <li className="nav-item">
-                <a href="/home" className="nav-link" onClick={logOut}>
-                    Cerrar Sesión
-                </a>
-                </li>
-            </div>
-            ) : (
-            <div className="navbar-nav ml-auto">
-                <li className="nav-item">
-                <Link to={"/home"} className="nav-link">
-                    Login
-                </Link>
-                </li>
-=======
           {currentUser && (
             <li className="nav-item">
               <Link to={'/user'} className="nav-link">
@@ -157,7 +100,6 @@ const NavBar = () => {
                 Login
               </Link>
             </li>
->>>>>>> 4827aa5a03443d463fefe8713dc5a3d64dadfa81
 
             <li className="nav-item">
               <Link to={'/home'} className="nav-link">
