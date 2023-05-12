cf9621ea9dc0e4a92f9ef9880f83e3f94d5cea9f
Daniel Diaz <danieldiaz-123@hotmail.com>
2023-05-11 23:30:10 -0300

    hotfix Topbar & Detail

--- Client/src/views/Detail/Detail.jsx
+++ Client/src/views/Detail/Detail.jsx
@@ -95,14 +95,14 @@ function Detail({ cart, setCart }) {
           <p className={styles.extra_p}>Sin puntuación</p>
           <p className={styles.price}>US ${detailProduct.price}</p>
           <div className={styles.quantity}>            
-            <button onClick={handleIncrement} className={styles.bottone5}>
-              {' '}
-              +{' '}
-            </button>
-            <span>{quantity}</span>
             <button onClick={handleDecrement} className={styles.bottone5}>
               {' '}
               -{' '}
+            </button>
+            <span>{quantity}</span>
+            <button onClick={handleIncrement} className={styles.bottone5}>
+              {' '}
+              +{' '}
             </button>
           </div>
           <p className={styles.extra_p}>Stock disponible: {detailProduct.stock}</p>
