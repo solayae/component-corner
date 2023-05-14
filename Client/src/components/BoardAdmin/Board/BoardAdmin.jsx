d2cedb9fcbea8a1b03776b81f43acc2175c2ad3c
Franco Inca <francoinca1604@gmail.com>
2023-05-13 19:24:45 -0500

    feat(BoardAdmin): Disabled admin for non-admin users.

--- Client/src/components/BoardAdmin/Board/BoardAdmin.jsx
+++ Client/src/components/BoardAdmin/Board/BoardAdmin.jsx
@@ -1,16 +1,27 @@
 import styles from "./BoardAdmin.module.css"
 import LeftPanel from "../LeftPanel/LeftPanel";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import InfoUsers from "../InfoUsers/InfoUsers";
+import { useNavigate } from "react-router-dom";
 
 const BoardAdmin = () => {
   const [viewType, setType] = useState("user")
+  const [isAdmin, setIsAdmin] = useState(false)
+  const user = JSON.parse(localStorage.getItem('user'));
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (user.roles.includes("RoleADMIN")) return setIsAdmin(true)
+    window.alert("No se cuentan con los privilegios necesarios.")
+    navigate("/")
+    //eslint-disable-next-line
+  }, [])
 
   return (
-    <div className={styles.boardAdmin_container}>
+    isAdmin &&
+    (<div className={styles.boardAdmin_container}>
       <LeftPanel setType={setType} />
       {viewType === "user" ? <InfoUsers /> : ""}
-    </div>
+    </div>)
   );
 }
 
