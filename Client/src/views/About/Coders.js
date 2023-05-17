f8019985ef0455762f44aa2170b32bb5d23a7999
Sebas Olaya <sebastianolayae@gmail.com>
2023-05-16 21:06:10 -0500

    about terminado, faltan algunas fotos

--- Client/src/views/About/Coders.js
+++ Client/src/views/About/Coders.js
@@ -1,12 +1,61 @@
 const coders = {
-  devs: [
-    { id: 1, nickname: 'John', name: 'Juan', img: 'imagen', linkedin: 'linkedin', github: 'github' },
-    { id: 2, nickname: 'Jane', name: 'Juan', img: 'imagen', linkedin: 'linkedin', github: 'github' },
-    { id: 3, nickname: 'Jane', name: 'Juan', img: 'imagen', linkedin: 'linkedin', github: 'github' },
-    { id: 4, nickname: 'Jane', name: 'Juan', img: 'imagen', linkedin: 'linkedin', github: 'github' },
-    { id: 2, nickname: 'Jane', name: 'Juan', img: 'imagen', linkedin: 'linkedin', github: 'github' },
-    { id: 2, nickname: 'Jane', name: 'Juan', img: 'imagen', linkedin: 'linkedin', github: 'github' },
-    { id: 2, nickname: 'Jane', name: 'Juan', img: 'imagen', linkedin: 'linkedin', github: 'github' },
+  data: [
+    {
+      id: 1,
+      name: 'Elias Roldan',
+      img: './assets/elias.png',
+      linkedin: 'https://www.linkedin.com/in/elias-f-roldan/',
+      github: 'https://github.com/RoldanElias',
+    },
+    {
+      id: 2,
+      name: 'Franco Inca',
+      img: './assets/franco.png',
+      linkedin: 'https://www.linkedin.com/in/franco-jes%C3%BAs-inca-donayre-7906b2262/',
+      github: 'https://github.com/FrancoInca',
+    },
+    {
+      id: 3,
+      name: 'Guido Fraticelli',
+      img: './assets/guido.png',
+      linkedin: 'https://www.linkedin.com/in/guido-fraticelli-481a3b242/',
+      github: 'https://github.com/guidofrati',
+    },
+    {
+      id: 4,
+      name: 'Sebastián Olaya',
+      img: './assets/sebastian.png',
+      linkedin: 'https://www.linkedin.com/in/solayae/',
+      github: 'https://github.com/solayae',
+    },
+    {
+      id: 5,
+      name: 'Carlos Carabajal',
+      img: './assets/carlos.png',
+      linkedin: 'https://www.linkedin.com/in/carlos-juan-carabajal-arga%C3%B1araz-903752225/',
+      github: 'https://github.com/carjuancara',
+    },
+    {
+      id: 6,
+      name: 'Daniel Diaz',
+      img: './assets/perrito.png',
+      linkedin: 'https://www.linkedin.com/in//',
+      github: 'https://github.com/DielanZH',
+    },
+    {
+      id: 7,
+      name: 'Billy Andrys',
+      img: './assets/perrito.png',
+      linkedin: 'https://www.linkedin.com/in//',
+      github: 'https://github.com/billyandrys',
+    },
+    {
+      id: 8,
+      name: 'Alejandro Medina',
+      img: './assets/perrito.png',
+      linkedin: 'https://www.linkedin.com/in/alejandro-medina-b4558419b/',
+      github: 'https://github.com/httpsAlejandroM',
+    },
   ],
 };
 
