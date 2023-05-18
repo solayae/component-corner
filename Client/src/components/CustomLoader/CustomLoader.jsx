37e8b053f3fd4111e87f44d6198c154754c80a8a
Sebas Olaya <sebastianolayae@gmail.com>
2023-05-17 22:02:30 -0500

    lazy loading ok

---
+++ Client/src/components/CustomLoader/CustomLoader.jsx
@@ -0,0 +1,16 @@
+import styles from './CustomLoader.module.css';
+
+export default function CustomLoader() {
+  return (
+    <div className={styles.containerLoader}>
+      <div className={styles.wrapper}>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.shadow}></div>
+        <div className={styles.shadow}></div>
+        <div className={styles.shadow}></div>
+      </div>
+    </div>
+  );
+}
